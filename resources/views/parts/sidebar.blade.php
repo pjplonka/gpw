06da2db981101633152b1fa8ee3d07c38b49403c
Paweł Płonka <pjplonka@gmail.com>
2021-05-02 16:15:20 +0200

    Add basic layout.

---
+++ resources/views/parts/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="aside">
+
+    <div class="top">
+        <span>GPW</span>
+    </div>
+
+    <ul>
+        <li>
+            <a href="/">
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="">
+                Logout
+            </a>
+        </li>
+    </ul>
+</div>
